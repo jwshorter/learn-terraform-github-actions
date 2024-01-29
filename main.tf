81bcf4dce013e3fccaca56dc2708b899b06a0897
Joseph Shorter <jwshorter@gmail.com>
2024-01-29 11:33:16 -0600

    this is a test change#

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "FluidGenius"
+    organization = "FluidGenius.com"
 
     workspaces {
       name = "demo-github-actions"
